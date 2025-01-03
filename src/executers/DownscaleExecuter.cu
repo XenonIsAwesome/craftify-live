28efb09113756b8992d5923ac1fd2d9ca843ef2e
Xenon <ofek171202@gmail.com>
2025-01-03 17:45:32 +0200

    namespace fix

--- src/executers/DownscaleExecuter.cu
+++ src/executers/DownscaleExecuter.cu
@@ -1,43 +1,45 @@
 #include "DownscaleExecuter.cuh"
 #include "../kernels/downscale.cuh"
 
-namespace craftify::executers {
-    std::optional<std::shared_ptr<cv::Mat>>
-    DownscaleExecuter::execute(std::shared_ptr<cv::Mat> input) {
-        unsigned int inputWidth = input->cols;
-        unsigned int inputHeight = input->rows;
-        unsigned int outputWidth = inputWidth / m_scale_factor;
-        unsigned int outputHeight = inputHeight / m_scale_factor;
+namespace craftify {
+    namespace executers {
+        std::shared_ptr<cv::Mat>
+        DownscaleExecuter::execute(std::shared_ptr<cv::Mat> input) {
+            unsigned int inputWidth = input->cols;
+            unsigned int inputHeight = input->rows;
+            unsigned int outputWidth = inputWidth / m_scale_factor;
+            unsigned int outputHeight = inputHeight / m_scale_factor;
 
-        auto output = std::make_shared<cv::Mat>(outputHeight, outputWidth, input->type());
+            auto output = std::make_shared<cv::Mat>(outputHeight, outputWidth, input->type());
 
-        uchar4 *d_input, *d_output;
-        size_t inputSize = inputWidth * inputHeight * sizeof(uchar4);
-        size_t outputSize = outputWidth * outputHeight * sizeof(uchar4);
+            uchar4 *d_input, *d_output;
+            size_t inputSize = inputWidth * inputHeight * sizeof(uchar4);
+            size_t outputSize = outputWidth * outputHeight * sizeof(uchar4);
 
-        cudaMalloc(&d_input, inputSize);
-        cudaMalloc(&d_output, outputSize);
+            cudaMalloc(&d_input, inputSize);
+            cudaMalloc(&d_output, outputSize);
 
-        cudaMemcpy(d_input, input->data, inputSize, cudaMemcpyHostToDevice);
+            cudaMemcpy(d_input, input->data, inputSize, cudaMemcpyHostToDevice);
 
-        dim3 blockSize(16, 16);
-        dim3 gridSize((outputWidth + blockSize.x - 1) / blockSize.x, (outputHeight + blockSize.y - 1) / blockSize.y);
+            dim3 blockSize(16, 16);
+            dim3 gridSize((outputWidth + blockSize.x - 1) / blockSize.x, (outputHeight + blockSize.y - 1) / blockSize.y);
 
-        craftify::kernels::downscale_image<<<gridSize, blockSize>>>(
-            d_input, d_output, 
-            inputWidth, inputHeight, 
-            outputWidth, outputHeight, 
-            m_scale_factor);
-        
-        cudaMemcpy(output->data, d_output, outputSize, cudaMemcpyDeviceToHost);
+            craftify::kernels::downscale_image<<<gridSize, blockSize>>>(
+                d_input, d_output, 
+                inputWidth, inputHeight, 
+                outputWidth, outputHeight, 
+                m_scale_factor);
+            
+            cudaMemcpy(output->data, d_output, outputSize, cudaMemcpyDeviceToHost);
 
-        cudaFree(d_input);
-        cudaFree(d_output);
+            cudaFree(d_input);
+            cudaFree(d_output);
 
-        cudaDeviceSynchronize();
+            cudaDeviceSynchronize();
 
-        return output;
-    }
+            return output;
+        }
 
-    DownscaleExecuter::DownscaleExecuter(unsigned int scale_factor): m_scale_factor(scale_factor) {}
-};
+        DownscaleExecuter::DownscaleExecuter(unsigned int scale_factor): m_scale_factor(scale_factor) {}
+    }; // namespace executers
+}; // namespace craftify
--- src/executers/DownscaleExecuter.cuh
+++ src/executers/DownscaleExecuter.cuh
@@ -4,17 +4,18 @@
 #include "DeviceExecuter.cuh"
 #include <opencv2/opencv.hpp>
 
-namespace craftify::executers {
-    class DownscaleExecuter : public DeviceExecuter<cv::Mat, cv::Mat>{
-    public:
-        explicit DownscaleExecuter(unsigned int scale_factor = 16u);
+namespace craftify {
+    namespace executers {
+        class DownscaleExecuter : public DeviceExecuter<cv::Mat, cv::Mat>{
+        public:
+            explicit DownscaleExecuter(unsigned int scale_factor = 16u);
 
-        std::optional<std::shared_ptr<cv::Mat>> execute(std::shared_ptr<cv::Mat> input) override;
+            std::shared_ptr<cv::Mat> execute(std::shared_ptr<cv::Mat> input) override;
 
-    private:
-        unsigned int m_scale_factor;
-    };
-}
-
+        private:
+            unsigned int m_scale_factor;
+        };
+    }; // namespace executers
+}; // namespace craftify
 
 #endif //DOWNSCALEEXECUTER_CUH
