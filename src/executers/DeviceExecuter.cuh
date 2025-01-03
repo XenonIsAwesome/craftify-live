931e6c4f787e6af1811ffff044e42065ffcfb270
Xenon <ofek171202@gmail.com>
2025-01-03 17:45:01 +0200

    namespace fix and rm optional

--- src/executers/DeviceExecuter.cuh
+++ src/executers/DeviceExecuter.cuh
@@ -2,13 +2,15 @@
 #define DEVICEEXECUTER_H
 
 #include <memory>
-#include <optional>
 
-namespace craftify::executers {
-    template<typename InT, typename OutT>
-    class DeviceExecuter{
-    public:
-        virtual std::optional<std::shared_ptr<OutT>> execute(std::shared_ptr <InT> input) = 0;
-    };
-}
+namespace craftify {
+    namespace executers {
+        template<typename InT, typename OutT>
+        class DeviceExecuter{
+        public:
+            virtual std::shared_ptr<OutT> execute(std::shared_ptr <InT> input) = 0;
+        };
+    }; // namespace executers
+}; // namespace craftify
+
 #endif //DEVICEEXECUTER_H
